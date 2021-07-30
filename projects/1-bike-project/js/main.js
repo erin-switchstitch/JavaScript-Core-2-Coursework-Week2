d7d579ab4041888f587262c5a355bde83fa8f8d4
erin-switchstitch <83361880+erin-switchstitch@users.noreply.github.com>
2021-07-31 00:00:19 +0100

    V.1.2
    Completed 1-bike-project, except for the second form part of the project

--- projects/1-bike-project/js/main.js
+++ projects/1-bike-project/js/main.js
@@ -1,115 +1,115 @@
-// id=blueBtn
 let cl = console.log
 
-function clickButton (buttonColorVariable, target) {
-
-   const colorChangeArray = [
+const colorChangeArray = [
     {
         buttonColor : "blue", 
-        "backgroundColor" : [{
-                target1 : `#588fbd`,
-                target2 : `#ffa500`,
-                target3 : `black`,
-            }],
-        "color" : [{
-                target1 : `white`,
-        }], 
+        "backgroundColor" : [
+                {target1 : `#72befb`},
+                {target2 : `#234e72`},
+                {target3 : `blue`},
+            ],
+        "color" : [
+                {target1 : `white`},
+        ], 
     },
     {
         buttonColor : "orange", 
-        
-        "backgroundColor" : [{
-                target1 : `#588fbd`,
-                target2 : `#ffa500`,
-                target3 : `black`,
-            }],
-            "color" : [{
-                target3 : `white`,
-            }], 
+        "backgroundColor" : [
+                {target1 : `#533e18`},
+                {target2 : `#ffa500`},
+                {target3 : `orange`},
+            ],
+        "color" : [
+                {target1 : `white`},
+        ], 
     },
     {
         buttonColor : "green", 
-        
-        "backgroundColor" : [{
-                target1 : `#588fbd`,
-                target2 : `#ffa500`,
-                target3 : `black`,
-            }],
-            "color" : [{
-                target3 : `white`,
-            }], 
-    }
+        "backgroundColor" : [
+                {target1 : `#2c9b4d`},
+                {target2 : `#245834`},
+                {target3 : `green`},
+            ],
+        "color" : [
+                {target1 : `white`},
+        ], 
+    },
 ];
+// cl(colorChangeArray);
+// cl(colorChangeArray[0]);
+// cl(colorChangeArray[0].buttonColor);
 
+
+
+function clickButton (buttonColorVariable, targetArrayInput) {
     const btnId = document.getElementById(`${buttonColorVariable}Btn`);
-    //cl(btnId);
-    //cl("selected color : " + buttonColorVariable);
-    
+        //cl("selected color : " + buttonColorVariable);
 
-    
     const buttonColorArray = [];
     for (let index = 0; index < colorChangeArray.length; index++) {
         buttonColorArray.push(colorChangeArray[index].buttonColor);
     }
     //cl(buttonColorArray);
+    //cl(buttonColorArray.indexOf(buttonColorVariable));
     let colorIndex = buttonColorArray.indexOf(buttonColorVariable);
-    //cl("colorIndex : " + colorIndex)
-    const currentArray = colorChangeArray[colorIndex];
+    const currentMainArray = colorChangeArray[colorIndex];
+    //cl(currentMainArray);
+    //cl(currentMainArray.backgroundColor[0]);
+
     let cssSelectors = Object.keys(colorChangeArray[colorIndex]);
     //cl("cssSelector :" + cssSelectors)
-    //cl(currentArray.backgroundColor[0].target1)
 
 
-    const propertyKeysArray = [];
-    const propertyValuesArray = [];
+    for (let i=1 ; i < (cssSelectors.length) ; i++){
+        const propertyKeysArray = [];
+        const propertyValuesArray = [];
 
-    for (let i=1 ; i < (cssSelectors.length+1) ; i++){
+        //cl(cssSelectors);
+        //cl(cssSelectors[i]);
+        const newSelector = cssSelectors[i];
         
-        propertyKeysArray.push(Object.keys(colorChangeArray[colorIndex]));
-        propertyValuesArray.push(Object.values(colorChangeArray[colorIndex]));
-        //cl("propertyKeysArray : " + propertyKeysArray[1]);
-        const forEachPropertyKeys = [];
-        const forEachPropertyValues = [];
+        // cl(currentMainArray[newSelector]);
 
-        propertyKeysArray.forEach(function(key,index){
-            const forEachProperty = key[index];
+        for (let index = 0 ; index < currentMainArray[newSelector].length ; index++ ){
+            propertyKeysArray.push(Object.keys(currentMainArray[newSelector][index]))
+            propertyValuesArray.push(Object.values(currentMainArray[newSelector][index]))
+            //cl(propertyKeysArray)
+            //cl(propertyValuesArray)
             
-            //cl("key : " + colorChangeArray[colorIndex][forEachProperty]);
-
-            if ( key[index] != "buttonColor") {
-                forEachPropertyKeys.push(Object.keys(colorChangeArray[colorIndex]));
-                let toChangeKeys = Object.keys(colorChangeArray[colorIndex][forEachProperty][0])[index];
-                let toChangeValues = (Object.values(colorChangeArray[colorIndex][forEachProperty][0])[index]);
-                cl((toChangeKeys) + " " + (toChangeValues));
-                cl(typeof(toChangeKeys) + " " + typeof(toChangeValues));
-                cl("forEachProperty :" + forEachProperty);
-                //forEachPropertyValues.push(Object.values(colorChangeArray[colorIndex][forEachProperty]));
-                //cl(index);
-
-                // I don't know how to access the 
-
-                let elemTarget = target[0];
-                cl(elemTarget)
-
-                [target[0]].style[forEachProperty] = toChangeValues;
-                //[`${toChangeKeys}[0]`].style[forEachProperty] = toChangeValues;
-                //cl($target1);
+            for (let j=0 ; j < propertyKeysArray.length ; j++ ){
+                //cl(propertyKeysArray);
+                const propertyKeysArrayIndex = propertyKeysArray[j][0];
+                    //cl("Current Target : " + propertyKeysArrayIndex);
+                const propertyValuesArrayIndex = propertyValuesArray[j][0];
+                    //cl("CSS New Selector: " + newSelector);
+                    //cl("CSS New Value  : " + propertyValuesArrayIndex);
+                const targetNumArray = ["target1","target2","target3"]
+                const numToTarget = targetNumArray.indexOf(propertyKeysArrayIndex);
+                const currentTargetElement = targetArrayInput[numToTarget];
+                    //cl(currentTargetElement);
+                currentTargetElement.style[newSelector] = `${propertyValuesArrayIndex}`;
             }
-            
-        })
-
-        //cl(forEachPropertyKeys);
-        //cl(forEachPropertyValues);
-       
-
-     
+        }
     }
-    
+}
+
+const target1 = document.getElementsByClassName("jumbotron")[0];
+const target2 = document.getElementsByClassName("btn btn-primary btn-lrg")[0];
+const target3 = document.getElementsByClassName("btn btn-secondary btn-lrg")[0];
+const targetArray = [target1, target2, target3];
+
+const colorButtonArray = document.getElementsByClassName("colorButton");
+for (let index = 0; index < colorButtonArray.length; index++) {
+    const element = colorButtonArray[index];
+
+    element.addEventListener("click",function(){
+        const rawColor = element.innerHTML
+        const outputColor = rawColor.toLowerCase();
+        cl(outputColor);
+        clickButton(outputColor,targetArray);
+    })
 }
 
 
-const target1 = document.getElementsByClassName("jumbotron");
-const target2 = document.getElementsByClassName("btn btn-primary btn-lrg")[0];
-const target3 = document.getElementsByClassName("btn btn-secondary btn-lrg")[0];
-
-clickButton("blue",target1);
+//need to create a function that inputs the color based on which element is clicked on. Could also use refactoring the 
+//code around targets 
